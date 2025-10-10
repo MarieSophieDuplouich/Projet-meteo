a6206471a67f85399b2fd6296a971a6c1ec73f01
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-10-10 10:33:17 +0200

    ça commence à afficher les données de géolocalisation

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,30 @@
-console.log("helloworld");
-
-
-
-
 
 //mettre le fetch
+function essai(){
+   fetch("https://geocoding-api.open-meteo.com/v1/search?name=paris&count=100&language=fr&format=json&countryCode=FR")
+        .then(response_obj => response_obj.json())
+        .then(positions_arr => {
+
+            const container = document.querySelector(".position-user");
+            const template_position = document.querySelector(".template-position");
+       //le "results" c'est ce qu'il ya dans l'api L’API Open-Meteo Geocoding ne renvoie pas directement un tableau, mais un objet JSON avec une clé results qui contient le tableau.
+         Array.prototype.forEach.call(positions_arr.results, position_obj => {
+                console.log(position_obj.name);
+                console.log(position_obj.latitude);
+                console.log(position_obj.longitude);
+
+                const position_elem = template_position.content.cloneNode(true);
+                position_elem.querySelector(".user-name").textContent = position_obj.name;
+                position_elem.querySelector(".longitude").textContent = position_obj.longitude;
+                position_elem.querySelector(".altitude").textContent = position_obj.latitude;
+                // position_elem.querySelector("img").alt = pokemon_obj.name;
+                container.appendChild(position_elem);
+            });
+        });
+
+}
+
+essai();
 
 // function searchBar_func{
 // position gps latitude longitude 
@@ -34,17 +54,17 @@ console.log("helloworld");
 
 // } 
 
-navigator.geolocation.watchPosition(async (position) => {
-    const { latitude, longitude } = position.coords;
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log('Météo actuelle:', data.current_weather);
-});
+// navigator.geolocation.watchPosition(async (position) => {
+//     const { latitude, longitude } = position.coords;
+//     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log('Météo actuelle:', data.current_weather);
+// });
 
 //deux liens que j'ai trouvé dans l'api
 
-https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,sunset,sunrise,rain_sum,temperature_2m_max,apparent_temperature_max,temperature_2m_min,daylight_duration,sunshine_duration,precipitation_probability_max,precipitation_hours,precipitation_sum,snowfall_sum,showers_sum,uv_index_clear_sky_max,uv_index_max&hourly=temperature_2m,sunshine_duration,is_day,uv_index_clear_sky,wet_bulb_temperature_2m,total_column_integrated_water_vapour,uv_index&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,surface_pressure,pressure_msl,cloud_cover,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,sunset,sunrise,rain_sum,temperature_2m_max,apparent_temperature_max,temperature_2m_min,daylight_duration,sunshine_duration,precipitation_probability_max,precipitation_hours,precipitation_sum,snowfall_sum,showers_sum,uv_index_clear_sky_max,uv_index_max&hourly=temperature_2m,sunshine_duration,is_day,uv_index_clear_sky,wet_bulb_temperature_2m,total_column_integrated_water_vapour,uv_index&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,surface_pressure,pressure_msl,cloud_cover,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m
 
 
-https://geocoding-api.open-meteo.com/v1/search?name=paris&count=100&language=fr&format=json&countryCode=FR
+// https://geocoding-api.open-meteo.com/v1/search?name=paris&count=100&language=fr&format=json&countryCode=FR
